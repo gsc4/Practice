e7ae975a25801faa35f1474be39b82a0ba19bd6f
gsc4 <gsc4@rice.edu>
2016-03-14 02:44:05 -0500

    Squished bugs and made the code produce desired graphs
    Still need to modify it such that intermediate points can be
    interpolated and solved

--- melting_curve.py
+++ melting_curve.py
@@ -1,40 +1,88 @@
 import numpy as np
-import pymbar 
-
+import pymbar
+import matplotlib
+matplotlib.use("Agg") 
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
+
+    #### READ FILES, SET CONSTANTS, AND GET THINGS READY ####    
+
+    # My boy boltzmann has a very special numberr
     kb = 0.0083145  # kJ/mol K
 
+    # Collect list of temperatures from file
     temps = [ x.rstrip("\n") for x in open("short_temps", "r").readlines() ]
     
-    T = [ float(x) for x in temps ] 
-    
-    num_interpolations = 0
+    # Turn list of temperatures into array of floats
+    T = np.array(temps, float) 
 
-    # I made usecols be a list because it was mad when I was just supplying an int
+    # Get energies from Etot.xvg files use these to create vector N_k
     energies = [ np.loadtxt("{}_0/Etot.xvg".format(x), usecols=[1]) for x in temps ]
-    N_k = [ len(x) for x in energies ]
-    Etot = np.concatenate(energies) 
+    N_k = np.array([ len(x) for x in energies ])
+    
+    # Put energies in array named Etot 
+    Etot = np.array(energies, float)
+    
+    # Read Q values for calculations later
+    Q = [np.loadtxt("{}_0/Q.dat".format(x.rstrip("\n"))) for x in temps] 
+    Q_kln = np.empty((len(T), Etot.shape[0], Etot.shape[1]), float)
+    Q_kln[:] = np.array(Q, float)    
 
-    u_kn = np.zeros((len(T), len(Etot)), float)
+    # Beta for calculating reduced potentials
+    B = 1/(kb*T) 
 
-    #for loop to fill in u_kn
+    # Initialize vector to hold reduced potentials
+    u_kln = np.zeros((len(T), Etot.shape[0], Etot.shape[1]), float)    
+    
+    # Loop to fill in u_kln
     for i in range(len(T)):
-        B = 1/(kb*T[i])
-        u_kn[i] = Etot*B
+        u_kln[i] = Etot*B[i]    
 
-    print len(T)
-    print len(Etot)
-    print u_kn.shape
-    print len(N_k) 
+    # Initialize mbar
+    mbar = pymbar.mbar.MBAR(u_kln, N_k)
+    
+    #### COMPUTE OBSERVABLES ####
+    
+    # For some reason we need to use unreduced potentials, aka Etot over and over
+    E_kln = np.empty((len(T), Etot.shape[0], Etot.shape[1]), float)
+    E_kln[:] = Etot
+    
+    # Calculate expectations for E
+    (Eavg, dEavg) = mbar.computeExpectations(E_kln)
 
-    mbar = pymbar.MBAR(u_kn, N_k)
-    print mbar
-    # compute observables
+
+    # Calculate expectations for E^2
+    (E2avg, dE2avg) = mbar.computeExpectations(E_kln**2)
+
 
     # Cv = (1/kb*T^2)*( <E^2> - <E>^2)
-    
+    Cv = kb*B**2*(E2avg - Eavg**2)   
+
 
     # <Q>
+    (Qavg, dQavg) = mbar.computeExpectations(Q_kln)
+
 
     # <A> = A(T)
+    # didn't do this one
+   
+    #### MAKE GRAPHS ####
+    
+    # Generate plot of Cv vs T
+    plt.plot(T,Cv)
+    plt.title('Cv vs T')
+    plt.xlabel('T (K)')
+    plt.ylabel('Cv')
+
+    plt.savefig("Cv vs T")
+    
+    plt.clf()
+ 
+    # Generate plot of Q vs T
+    plt.plot(T,Qavg)
+    plt.title('Q vs T')
+    plt.xlabel('T (K)')
+    plt.ylabel('Q')
+    plt.savefig("Q vs T")
+
